0290e5edffc682da7c31ef10dc2ae2565a46de72
tokoi <trustsport12quruli@yahoo.co.jp>
2020-04-11 00:56:15 +0900

    画面遷移処理

--- app/src/main/java/com/example/intentsample2/MainActivity.kt
+++ app/src/main/java/com/example/intentsample2/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.intentsample2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
 
@@ -19,6 +22,21 @@ class MainActivity : AppCompatActivity() {
 
         menuList.add(menu)
 
+        menu = mutableMapOf("name" to "ヒレカツ定食", "price" to "900円" )
+
+        menuList.add(menu)
+
+
+        menu = mutableMapOf("name" to "刺身定食", "price" to "950円" )
+
+        menuList.add(menu)
+
+
+        menu = mutableMapOf("name" to "焼き魚定食", "price" to "700円" )
+
+        menuList.add(menu)
+
+
         val from = arrayOf("name", "price")
 
         val to = intArrayOf(android.R.id.text1, android.R.id.text2)
@@ -26,5 +44,36 @@ class MainActivity : AppCompatActivity() {
         val adapter = SimpleAdapter(applicationContext, menuList, android.R.layout.simple_list_item_2, from, to)
 
         lvMenu.adapter = adapter
+
+        lvMenu.onItemClickListener = ListItemClickListner()
+
+
+    }
+
+    private inner class ListItemClickListner : AdapterView.OnItemClickListener{
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+            if(parent != null){
+
+                val item = parent.getItemAtPosition(position) as MutableMap<String, String>
+
+                val menuName = item["name"]
+                val menuPrice = item["price"]
+
+                val intent = Intent(applicationContext, MenuThanksActivity::class.java)
+
+                intent.putExtra("menuName", menuName)
+                intent.putExtra("menuPrice", menuPrice)
+
+                startActivity(intent)
+
+            }
+
+
+
+
+        }
+
+
     }
 }
